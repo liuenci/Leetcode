dc30ed4f4560ec3be38e67bf19a50646ab664270
liuenci <liuencier@gmail.com>
2018-12-04 21:54:59 +0800

    Implement Queue using Stacks

---
+++ src/com/cier/solution/StackAndQueue/MyQueue.java
@@ -0,0 +1,44 @@
+package com.cier.solution.StackAndQueue;
+
+import java.util.Stack;
+
+// https://leetcode.com/problems/implement-queue-using-stacks/description/
+public class MyQueue {
+    private Stack<Integer> inStack = new Stack<>();
+    private Stack<Integer> outStack = new Stack<>();
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+        while (!outStack.isEmpty()){
+            inStack.push(outStack.pop());
+        }
+        inStack.push(x);
+    }
+
+    public int pop() {
+        while (!inStack.isEmpty()){
+            outStack.push(inStack.pop());
+        }
+        return outStack.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (outStack.isEmpty()){
+            if (inStack.isEmpty()){
+                return -1;
+            }
+            while (!inStack.isEmpty()){
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return outStack.empty() && inStack.empty();
+    }
+}
