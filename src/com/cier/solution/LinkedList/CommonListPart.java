5be9482f787a5b3046bc86e33913d43a5afcbfd1
liuenci <liuencier@gmail.com>
2018-12-15 19:58:51 +0800

    打印两个有序链表的公共部分

---
+++ src/com/cier/solution/LinkedList/CommonListPart.java
@@ -0,0 +1,22 @@
+package com.cier.solution.LinkedList;
+
+import com.cier.solution.common.Node;
+
+public class CommonListPart {
+
+    public void printCommonPart(Node head1, Node head2){
+        System.out.println("Common Part:");
+        while (head1 != null && head2 != null){
+            if (head1.value < head2.value){
+                head1 = head1.next;
+            } else if (head1.value > head2.value){
+                head2 = head2.next;
+            } else {
+                System.out.print(head1.value + " ");
+                head1 = head1.next;
+                head2 = head2.next;
+            }
+        }
+        System.out.println();
+    }
+}
---
+++ src/com/cier/solution/common/Node.java
@@ -0,0 +1,10 @@
+package com.cier.solution.common;
+
+public class Node {
+    public int value;
+    public Node next;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
