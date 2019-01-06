5d5836446973c8df7e24a41944c276de9fccb353
liuenci <liuencier@gmail.com>
2019-01-06 12:09:34 +0800

    count-and-say

---
+++ src/com/cier/solution/string/CountAndSay.java
@@ -0,0 +1,28 @@
+package com.cier.solution.string;
+
+// https://leetcode.com/problems/count-and-say/
+public class CountAndSay {
+    public String countAndSay(int n) {
+        if (n == 1){
+            return "1";
+        }
+        String s = countAndSay(n - 1);
+        int i = 0;
+        String out = "";
+        while (i < s.length()){
+            int j = i + 1;
+            while (j < s.length() && s.charAt(i) == s.charAt(j)){
+                j++;
+            }
+            out = out + (j - i) + s.charAt(i);
+            i = j;
+        }
+        return out;
+    }
+
+    public static void main(String[] args) {
+        CountAndSay countAndSay = new CountAndSay();
+        countAndSay.countAndSay(4);
+        System.out.println("end");
+    }
+}
