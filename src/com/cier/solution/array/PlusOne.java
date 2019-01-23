73b45aed555395c9293310a57637f48eba25f2a2
liuenci <liuencier@gmail.com>
2019-01-23 19:54:03 +0800

    Plus One

--- src/com/cier/solution/array/PlusOne.java
+++ src/com/cier/solution/array/PlusOne.java
@@ -2,16 +2,25 @@ package com.cier.solution.array;
 // https://leetcode.com/problems/plus-one/
 public class PlusOne {
     public int[] plusOne(int[] digits) {
-        StringBuffer stringBuffer = new StringBuffer();
-        for (int i = 0; i < digits.length; i++){
-            stringBuffer.append(digits[i]);
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9){
+                digits[i]++;
+                break;
+            } else {
+                digits[i] = 0;
+            }
         }
-        Long num = Long.valueOf(stringBuffer.toString()) + 1;
-        String str = String.valueOf(num);
-        int[] nums = new int[str.length()];
-        for (int i = 0; i < nums.length; i++){
-            nums[i] = str.charAt(i);
+        if (digits[0] == 0){
+            int[] res = new int[digits.length + 1];
+            res[0] = 1;
+            return res;
         }
-        return nums;
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        PlusOne plusOne = new PlusOne();
+        int[] res = plusOne.plusOne(new int[]{9,9,9});
+        System.out.println("end");
     }
 }
