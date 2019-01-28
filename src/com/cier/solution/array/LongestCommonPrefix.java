c628c0d096af88031ba4421d517caf72b808c917
liuenci <liuencier@gmail.com>
2019-01-28 13:57:39 +0800

    垂直扫描

--- src/com/cier/solution/array/LongestCommonPrefix.java
+++ src/com/cier/solution/array/LongestCommonPrefix.java
@@ -17,10 +17,24 @@ public class LongestCommonPrefix {
         }
         return prefix;
     }
-
+    // 垂直扫描
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
     public static void main(String[] args) {
         LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
-        longestCommonPrefix.longestCommonPrefix(new String[]{"flower","flow","flight"});
+        longestCommonPrefix.longestCommonPrefix2(new String[]{"flower","flow","flight"});
         System.out.println("end");
     }
 }
