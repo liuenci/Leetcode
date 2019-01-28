c75de6d567c5679be771b6efeeec3ba65a44aeca
liuenci <liuencier@gmail.com>
2019-01-28 14:59:53 +0800

    sum-of-two-integers

---
+++ src/com/cier/solution/math/SumOfTwoIntegers.java
@@ -0,0 +1,8 @@
+package com.cier.solution.math;
+// https://leetcode.com/problems/sum-of-two-integers/
+public class SumOfTwoIntegers {
+
+    public int getSum(int a, int b) {
+        return a+b;
+    }
+}
