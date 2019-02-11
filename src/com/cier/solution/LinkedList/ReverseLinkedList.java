4bfa577f5bcfa9744e408459c652e5058b810ad5
liuenci <liuencier@gmail.com>
2019-02-11 11:41:24 +0800

    reverse-linked-list

--- src/com/cier/solution/LinkedList/ReverseLinkedList.java
+++ src/com/cier/solution/LinkedList/ReverseLinkedList.java
@@ -4,6 +4,7 @@ import com.cier.solution.common.ListNode;
 
 // https://leetcode.com/problems/reverse-linked-list/
 public class ReverseLinkedList {
+    // 迭代
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
@@ -14,5 +15,15 @@ public class ReverseLinkedList {
             curr = tempNode;
         }
         return prev;
+    }
+    // 递归
+    public ListNode reverseList1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = reverseList1(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
     }
 }
