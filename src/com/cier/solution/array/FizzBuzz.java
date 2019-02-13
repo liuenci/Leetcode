3becc4f97b4d50de62178974881d392d10de98e1
liuenci <liuencier@gmail.com>
2019-02-13 21:10:03 +0800

    fizz-buzz

---
+++ src/com/cier/solution/array/FizzBuzz.java
@@ -0,0 +1,25 @@
+package com.cier.solution.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/fizz-buzz/solution/
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            boolean divBy3 = (i % 3 == 0);
+            boolean divBy5 = (i % 5 == 0);
+            if (divBy3 && divBy5) {
+                result.add("FizzBuzz");
+            } else if (divBy3) {
+                result.add("Fizz");
+            } else if (divBy5) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+}
