7c5f1be0b56169e3b6be05d3cc84cb361d3856ba
liuenci <liuencier@gmail.com>
2019-06-23 22:19:40 +0800

    merge two sorted lists

--- src/com/cier/solution/sort/MergeTwoSortedLists.java
+++ src/com/cier/solution/sort/MergeTwoSortedLists.java
@@ -2,7 +2,9 @@ package com.cier.solution.sort;
 
 import com.cier.solution.common.ListNode;
 
-// https://leetcode.com/problems/merge-two-sorted-lists/
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/
+ */
 public class MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
@@ -19,5 +21,28 @@ public class MergeTwoSortedLists {
             l2.next = mergeTwoLists(l2.next,l1);
             return l2;
         }
+    }
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode prev = result;
+        // 遍历两个链表，每次比较链表头的大小，每次让较小值添加到 dummy 的后面，并且让较小值所在的链表后移一位
+        while (l1!= null && l2!= null) {
+            if (l1.val >= l2.val) {
+                prev.next = l2;
+                l2 = l2.next;
+            } else {
+                prev.next = l1;
+                l1 = l1.next;
+            }
+            prev = prev.next;
+        }
+        // 会出现一条链表遍历完另外一条链表没遍历完的情况，需要将没遍历的链表添加到结果链表中
+        if (l1 != null) {
+            prev.next = l1;
+        }
+        if (l2 != null) {
+            prev.next = l2;
+        }
+        return result.next;
     }
 }
