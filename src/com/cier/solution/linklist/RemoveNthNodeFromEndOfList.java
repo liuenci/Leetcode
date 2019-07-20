064b63876a4b23e72d66e76543f689984896a326
liuenci <liuencier@gmail.com>
2019-07-20 20:26:36 +0800

    update

---
+++ src/com/cier/solution/linklist/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,24 @@
+package com.cier.solution.linklist;
+
+import com.cier.solution.common.ListNode;
+
+/**
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ */
+public class RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
