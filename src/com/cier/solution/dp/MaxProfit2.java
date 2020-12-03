64abf6fb48263e75168809684ed704e694e8376a
liuenci <liuencier@gmail.com>
2020-12-03 20:19:46 +0800

    :sparkles: best-time-to-buy-and-sell-stock2

--- src/com/cier/solution/dp/MaxProfit2.java
+++ src/com/cier/solution/dp/MaxProfit2.java
@@ -10,7 +10,7 @@ public class MaxProfit2 {
 
     public static void main(String[] args) {
         MaxProfit2 maxProfit2 = new MaxProfit2();
-        int[] a = {7,1,5,3,6,4};
+        int[] a = {7, 1, 5, 3, 6, 4};
         System.out.println(maxProfit2.maxProfit(a));
     }
 
@@ -29,5 +29,16 @@ public class MaxProfit2 {
             }
         }
         return profit;
+    }
+
+    public int maxProfit2(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[prices.length - 1][0];
     }
 }
