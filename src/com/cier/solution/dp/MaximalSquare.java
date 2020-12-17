eb334e052b3810fa724fb03b61a24d5427e8bc78
liuenci <liuencier@gmail.com>
2020-12-17 19:41:12 +0800

    :sparkles: maximal-square

--- src/com/cier/solution/dp/MaximalSquare.java
+++ src/com/cier/solution/dp/MaximalSquare.java
@@ -41,9 +41,10 @@ public class MaximalSquare {
 
         for (int i = 1; i < rows; i++) {
             for (int j = 1; j < columns; j++) {
-                if (matrix[i][j] == '1')
-                dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
-                result = Math.max(result, dp[i][j]);
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    result = Math.max(result, dp[i][j]);
+                }
             }
         }
 
