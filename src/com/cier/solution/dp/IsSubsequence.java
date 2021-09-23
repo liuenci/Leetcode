83c77ca463ad960330645a6558ebb177fea83530
liuenci <liuencier@gmail.com>
2021-09-23 12:37:10 +0800

    :sparkles: IsSubsequence dp solution

--- src/com/cier/solution/dp/IsSubsequence.java
+++ src/com/cier/solution/dp/IsSubsequence.java
@@ -8,9 +8,15 @@ package com.cier.solution.dp;
  **/
 public class IsSubsequence {
 
+    public static void main(String[] args) {
+        IsSubsequence subsequence = new IsSubsequence();
+        System.out.println(subsequence.isSubsequence2("abc", "ahbgdc"));
+    }
+
     /**
      * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
      * 输入：s = "aa", t = "ba"
+     *
      * @param s
      * @param t
      * @return
@@ -34,8 +40,21 @@ public class IsSubsequence {
         return true;
     }
 
-    public static void main(String[] args) {
-        IsSubsequence subsequence = new IsSubsequence();
-        System.out.println(subsequence.isSubsequence("aa", "ba"));
+    public boolean isSubsequence2(String s, String t) {
+        if (s.length() == 0) {
+            return true;
+        }
+        int lowIndex = 0;
+        int highIndex = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (lowIndex > s.length() - 1) {
+                return true;
+            }
+            if (s.charAt(lowIndex) == t.charAt(highIndex)) {
+                lowIndex++;
+            }
+            highIndex++;
+        }
+        return lowIndex > s.length() - 1;
     }
 }
