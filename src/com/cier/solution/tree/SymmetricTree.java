2129443e30a19ca3a98645eaa898cbe7539e5e9b
liuenci <liuencier@gmail.com>
2021-11-14 17:30:18 +0800

    :sparkles: SymmetricTree tree

--- src/com/cier/solution/tree/SymmetricTree.java
+++ src/com/cier/solution/tree/SymmetricTree.java
@@ -37,4 +37,21 @@ public class SymmetricTree {
         }
         return true;
     }
+
+    public boolean isSymmetric1(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
 }
