7072fb43c820f298ee36d630c8ffb0e998fc3fd9
liuenci <liuencier@gmail.com>
2021-11-14 17:06:45 +0800

    :sparkles: InOrderTraversal tree

--- src/com/cier/solution/dp/NumWays.java
+++ src/com/cier/solution/dp/NumWays.java
@@ -9,12 +9,16 @@ package com.cier.solution.dp;
 public class NumWays {
 
     public int numWays(int n, int[][] relation, int k) {
+        // 定义动态规划的状态 dp[i][j], dp[i][j] 为经过 i 轮传递到编号 j 的玩家的方案数，其中 0 ≤ i ≤k，0≤j<n。
         int[][] dp = new int[k + 1][n];
+        // 0 传递到 0 也是一种方案
         dp[0][0] = 1;
+        // 通过遍历关系列表，对于(from, to)的关系，第k步到达to的方案数总是由第k-1步到达from的方案数总和。
         for (int i = 0; i < k; i++) {
             for (int[] edge : relation) {
                 int src = edge[0];
                 int dst = edge[1];
+                // dp[1][2] = dp[0][0] + dp[1][2] = 1;
                 dp[i + 1][dst] += dp[i][src];
             }
         }
--- src/com/cier/solution/tree/InOrderTraversal.java
+++ src/com/cier/solution/tree/InOrderTraversal.java
@@ -7,9 +7,11 @@ import java.util.List;
 
 /**
  * https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/
+ * 在二叉树中，中序遍历首先遍历左子树，然后访问根结点，最后遍历右子树。
  */
 public class InOrderTraversal {
     List<Integer> list = new ArrayList<>();
+
     public List<Integer> inorderTraversal(TreeNode root) {
         if (root == null) {
             return list;
@@ -18,5 +20,21 @@ public class InOrderTraversal {
         list.add(root.val);
         inorderTraversal(root.right);
         return list;
+    }
+
+
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        inorderTraversal1(root.left);
+        list.add(root.val);
+        inorderTraversal1(root.right);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        InOrderTraversal inOrderTraversal = new InOrderTraversal();
+        System.out.println(inOrderTraversal.inorderTraversal1(new TreeNode(1)));
     }
 }
