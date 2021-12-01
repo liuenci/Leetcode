4194da954e010cf0f58459dc6a847ae5e5f50a15
liuenci <liuencier@gmail.com>
2021-12-01 22:43:11 +0800

    :sparkles: MinDepth tree

--- src/com/cier/solution/tree/MinDepth.java
+++ src/com/cier/solution/tree/MinDepth.java
@@ -1,5 +1,7 @@
 package com.cier.solution.tree;
 
+import com.cier.solution.common.TreeNode;
+
 /**
  * @program: Leetcode
  * @description: 二叉树的最小深度
@@ -7,4 +9,17 @@ package com.cier.solution.tree;
  * @create: 2021-11-14 18:11
  **/
 public class MinDepth {
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right != null) {
+            return 1 + minDepth(root.right);
+        }
+        if (root.left != null && root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
 }
