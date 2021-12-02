0a1e2f265e001b125bcbc8bf9cc8a25a8d45023e
liuenci <liuencier@gmail.com>
2021-12-02 23:06:27 +0800

    :sparkles: LowestCommonAncestor tree

---
+++ src/com/cier/solution/tree/LowestCommonAncestor.java
@@ -0,0 +1,24 @@
+package com.cier.solution.tree;
+
+import com.cier.solution.common.TreeNode;
+
+/**
+ * @program: Leetcode
+ * @description: 二叉搜索树的最近公共祖先
+ * @author: liuenci
+ * @create: 2021-12-02 23:02
+ **/
+public class LowestCommonAncestor {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val < q.val) {
+            return root;
+        } else if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
