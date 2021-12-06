96640ac8cdf29423d2c6f2840d985358d38b711b
liuenci <liuencier@gmail.com>
2021-12-06 22:51:41 +0800

    :sparkles: SumOfLeftLeaves tree

--- src/com/cier/solution/tree/SumOfLeftLeaves.java
+++ src/com/cier/solution/tree/SumOfLeftLeaves.java
@@ -10,7 +10,25 @@ import com.cier.solution.common.TreeNode;
  **/
 public class SumOfLeftLeaves {
 
+    /**
+     * 1
+     * 2 3
+     * 4 5
+     * @param root
+     * @return
+     */
     public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int result = 0;
+        if (root.left != null && isLeafNode(root.left)) {
+            result += root.left.val;
+        }
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right) + result;
+    }
 
+    public boolean isLeafNode(TreeNode node) {
+        return node.left == null && node.right == null;
     }
 }
