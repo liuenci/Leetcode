9a76ae0b7eace9c81793bfa6e6ce67f90d278661
liuenci <liuencier@gmail.com>
2021-12-06 22:17:00 +0800

    :sparkles: SumOfLeftLeaves tree

---
+++ src/com/cier/solution/tree/SumOfLeftLeaves.java
@@ -0,0 +1,16 @@
+package com.cier.solution.tree;
+
+import com.cier.solution.common.TreeNode;
+
+/**
+ * @program: Leetcode
+ * @description: 左叶子之和
+ * @author: liuenci
+ * @create: 2021-12-06 22:12
+ **/
+public class SumOfLeftLeaves {
+
+    public int sumOfLeftLeaves(TreeNode root) {
+
+    }
+}
