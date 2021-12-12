76371ef71db8d718cdbbe5ed6256bc03387e5807
liuenci <liuencier@gmail.com>
2021-12-12 12:30:45 +0800

    :sparkles: [703] 数据流中的第 K 大元素

---
+++ src/com/cier/solution/tree/KthLargest.java
@@ -0,0 +1,32 @@
+package com.cier.solution.tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+/**
+ * @program: Leetcode
+ * @description: 数据流中的第 K 大元素
+ * @author: liuenci
+ * @create: 2021-12-12 11:47
+ **/
+public class KthLargest {
+    PriorityQueue<Integer> queue = new PriorityQueue<>();
+    int searchKey = 0;
+    public KthLargest(int k, int[] nums) {
+        searchKey = k;
+        for (int i = 0; i < nums.length; i++) {
+            add(nums[i]);
+        }
+    }
+
+    public int add(int val) {
+        queue.offer(val);
+        if (queue.size() > searchKey) {
+            queue.poll();
+        }
+        return queue.peek();
+    }
+}
