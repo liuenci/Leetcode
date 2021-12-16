bee40b82c1c4bdea6f5d38797cfb7101997780a8
“liuenci” <liuencier@gmail.com>
2021-12-16 12:08:25 +0800

    SumRootToLeaf

--- src/com/cier/solution/array/BestTimeToBuyAndSellStock.java
+++ src/com/cier/solution/array/BestTimeToBuyAndSellStock.java
@@ -1,11 +1,12 @@
 package com.cier.solution.array;
+
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/221860/A-really-easy-understand-java-method
 public class BestTimeToBuyAndSellStock {
 
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
-        for (int price:prices) {
+        for (int price : prices) {
             maxProfit = Math.max(maxProfit, price - minPrice);
             minPrice = Math.min(price, minPrice);
         }
@@ -14,7 +15,7 @@ public class BestTimeToBuyAndSellStock {
 
     public static void main(String[] args) {
         BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
-        bestTimeToBuyAndSellStock.maxProfit(new int[]{7,1,5,3,6,4});
+        bestTimeToBuyAndSellStock.maxProfit(new int[]{7, 1, 5, 3, 6, 4});
         System.out.println("end");
     }
 }
