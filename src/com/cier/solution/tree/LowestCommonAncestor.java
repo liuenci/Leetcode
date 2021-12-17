f5fedff2399b3685b36bd90995c568b716d0bd1e
liuenci <liuencier@gmail.com>
2021-12-18 00:06:51 +0800

    :sparkles: 二叉搜索树的最近公共祖先

--- src/com/cier/solution/tree/LowestCommonAncestor.java
+++ src/com/cier/solution/tree/LowestCommonAncestor.java
@@ -11,9 +11,7 @@ import com.cier.solution.common.TreeNode;
 public class LowestCommonAncestor {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root.val > p.val && root.val < q.val) {
-            return root;
-        } else if (root.val > p.val && root.val > q.val) {
+        if (root.val > p.val && root.val > q.val) {
             return lowestCommonAncestor(root.left, p, q);
         } else if (root.val < p.val && root.val < q.val) {
             return lowestCommonAncestor(root.right, p, q);
