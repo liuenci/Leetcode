1344431d12f6071af8c292faa90cf2950868cc0c
liuenci <liuencier@gmail.com>
2021-12-17 23:25:03 +0800

    :sparkles: 从上到下按层打印二叉树

--- src/com/cier/solution/tree/LevelOrder.java
+++ src/com/cier/solution/tree/LevelOrder.java
@@ -3,7 +3,9 @@ package com.cier.solution.tree;
 import com.cier.solution.common.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * https://leetcode.com/problems/binary-tree-level-order-traversal/description/
@@ -26,5 +28,28 @@ public class LevelOrder {
         res.get(level).add(root.val);
         dfs(res,root.left,level+1);
         dfs(res,root.right,level+1);
+    }
+
+    public List<List<Integer>> levelOrder1(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = queue.poll();
+                list.add(treeNode.val);
+                if (treeNode.left != null) {
+                    queue.offer(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.offer(treeNode.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
     }
 }
