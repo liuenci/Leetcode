b49d09571ed2f82a1951954057e11b80b586c0a7
liuenci <liuencier@gmail.com>
2021-12-22 23:54:56 +0800

    :sparkles: 柠檬水找零

--- src/com/cier/solution/greedy/LemonadeChange.java
+++ src/com/cier/solution/greedy/LemonadeChange.java
@@ -1,5 +1,8 @@
 package com.cier.solution.greedy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @program: Leetcode
  * @description: 柠檬水找零
@@ -9,6 +12,26 @@ package com.cier.solution.greedy;
 public class LemonadeChange {
 
     public boolean lemonadeChange(int[] bills) {
-
+        int five = 0;
+        int ten = 0;
+        for (int i : bills) {
+            if (i == 5) {
+                five++;
+            } else if (i == 10) {
+                five--;
+                ten++;
+            } else if (i == 20) {
+                if (ten > 0) {
+                    ten--;
+                    five--;
+                } else {
+                    five -= 3;
+                }
+            }
+            if (five < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
