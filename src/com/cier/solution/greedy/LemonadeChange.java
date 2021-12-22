210bb4b31fe4343ca2e11fb01bb18109cfbf2583
liuenci <liuencier@gmail.com>
2021-12-22 23:35:01 +0800

    :sparkles: 柠檬水找零

---
+++ src/com/cier/solution/greedy/LemonadeChange.java
@@ -0,0 +1,14 @@
+package com.cier.solution.greedy;
+
+/**
+ * @program: Leetcode
+ * @description: 柠檬水找零
+ * @author: liuenci
+ * @create: 2021-12-22 23:33
+ **/
+public class LemonadeChange {
+
+    public boolean lemonadeChange(int[] bills) {
+
+    }
+}
