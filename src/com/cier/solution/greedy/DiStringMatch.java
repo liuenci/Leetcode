cd04c38e6166e197a5a2986cd52610c23bc5a1ed
liuenci <liuencier@gmail.com>
2022-01-01 22:12:22 +0800

    :sparkles: [942]增减字符串匹配

--- src/com/cier/solution/greedy/DiStringMatch.java
+++ src/com/cier/solution/greedy/DiStringMatch.java
@@ -7,4 +7,22 @@ package com.cier.solution.greedy;
  * @create: 2021-12-23 22:29
  **/
 public class DiStringMatch {
+
+    public int[] diStringMatch(String s) {
+        int size = s.length() + 1;
+        int[] array = new int[size];
+        int max = s.length();
+        int min = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'I') {
+                array[i] = min;
+                min++;
+            } else {
+                array[i] = max;
+                max--;
+            }
+        }
+        array[size - 1] = max;
+        return array;
+    }
 }
