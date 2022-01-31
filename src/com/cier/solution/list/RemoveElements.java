26e15dcf22014b5a9ebcf43e1404f96c424c1e60
liuenci <liuencier@gmail.com>
2022-01-31 10:30:03 +0800

    :sparkles: [203] 移除链表元素添加注释

--- src/com/cier/solution/list/RemoveElements.java
+++ src/com/cier/solution/list/RemoveElements.java
@@ -17,11 +17,13 @@ public class RemoveElements {
      * @return
      */
     public ListNode removeElements(ListNode head, int val) {
+        // 核心是设置头节点，否则会出现连续相同的节点无法被删除
         ListNode header = new ListNode(-1);
         header.next = head;
         ListNode cur = header;
         while (cur.next != null) {
             if (cur.next.val == val) {
+                // 当两个节点的值相等时，只需将当前节点的指针指向下一个相同节点的下一个节点位置
                 cur.next = cur.next.next;
             } else {
                 cur = cur.next;
