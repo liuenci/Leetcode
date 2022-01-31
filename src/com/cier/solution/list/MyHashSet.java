f4d3a73d4fb381760171a5d08b6af6474686b21c
liuenci <liuencier@gmail.com>
2022-01-31 10:44:46 +0800

    :sparkles: [705] 设计哈希集合添加注释

--- src/com/cier/solution/list/MyHashSet.java
+++ src/com/cier/solution/list/MyHashSet.java
@@ -10,13 +10,19 @@ import java.util.List;
  */
 public class MyHashSet {
 
+    /**
+     * 无法使用内置的哈希集合来处理，那可以用 List 来实现一个哈希集合
+     */
     List<Integer> array;
-
 
     public MyHashSet() {
         array = new ArrayList<>();
     }
 
+    /**
+     * 添加 key 的时候不能直接 array.add(key); 要考虑本身 List 中是否存在相同的 key
+     * @param key
+     */
     public void add(int key) {
         for (Integer integer : array) {
             if (key == integer) {
@@ -26,6 +32,10 @@ public class MyHashSet {
         array.add(key);
     }
 
+    /**
+     * 移除 key 的时候不能直接 array.remove(i); 这里的 key 是 int 类型，直接 remove 会走到重载方法按照索引移除数据的地方
+     * @param key
+     */
     public void remove(int key) {
         for (int i = 0; i < array.size(); i++) {
             if (array.get(i) == key) {
