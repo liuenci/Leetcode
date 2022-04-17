29f4c1e6a6a267a5b8d27635bd0acf955780d514
liuenci <liuencier@gmail.com>
2022-04-18 00:05:47 +0800

    :sparkles: 无重复字符的最长子串增加注释

--- src/com/cier/solution/string/LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/cier/solution/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 /**
- * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ * 无重复字符的最长子串
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
  */
 public class LongestSubstringWithoutRepeatingCharacters {
     /**
