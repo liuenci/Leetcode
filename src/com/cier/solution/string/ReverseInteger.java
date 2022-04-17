70fb999964209061337ed5106d3b912ada9b129e
liuenci <liuencier@gmail.com>
2022-04-17 23:58:03 +0800

    :sparkles: [3] 无重复字符的最长子串更高效的解法

--- src/com/cier/solution/string/ReverseInteger.java
+++ src/com/cier/solution/string/ReverseInteger.java
@@ -5,13 +5,13 @@ public class ReverseInteger {
 
     public static int reverse(int x) {
         int rev = 0;
-        while (x != 0){
+        while (x != 0) {
             int pop = x % 10;
             x /= 10;
-            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE && pop > 7)){
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE && pop > 7)) {
                 return 0;
             }
-            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE && pop < -8)){
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE && pop < -8)) {
                 return 0;
             }
             rev = rev * 10 + pop;
@@ -22,7 +22,7 @@ public class ReverseInteger {
     public static void main(String[] args) {
         System.out.println(reverse(1534236469));
         System.out.println("end");
-        System.out.println(Math.pow(-2,31));
-        System.out.println(Math.pow(2,31) - 1);
+        System.out.println(Math.pow(-2, 31));
+        System.out.println(Math.pow(2, 31) - 1);
     }
 }
